2e720036fa7840ff1670b4f4218688d9215aef4a
Moazzem <tiyonbhuiyan.201@gmail.com>
2024-10-26 00:46:38 +0600

    update

---
+++ src/app/loading.js
@@ -0,0 +1,9 @@
+
+export default function Loading() {
+    return (
+      <div className=" flex justify-center items-center h-screen">
+       <span className="loading loading-spinner text-error text-3xl"></span>
+      </div>
+    );
+  }
+  
