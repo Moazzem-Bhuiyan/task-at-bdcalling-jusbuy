2d08d5d46d4fea936bbebac20b50fff040dfaceb
Moazzem <tiyonbhuiyan.201@gmail.com>
2024-10-26 00:51:27 +0600

    complete

--- src/app/Components/shared/WishButton.jsx
+++ src/app/Components/shared/WishButton.jsx
@@ -3,10 +3,15 @@ import { CiHeart } from 'react-icons/ci';
 
 
 const WishButton = () => {
+
+    const handlewish =()=>{
+
+        alert("Item added wish list")
+    }
     return (
         <div className='bg-white rounded-full flex justify-center p-1 hover:bg-primary hover:text-white'>
 
-            <button className='text-2xl'><CiHeart /></button>
+            <button onClick={handlewish} className='text-2xl'><CiHeart /></button>
             
         </div>
     );
